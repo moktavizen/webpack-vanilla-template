55ff068459793632c15363219c34dc54984934fa
Moh Oktavi Aziz Nugraha <moktavizzen@gmail.com>
2026-02-28 09:06:41 +0700

    First commit

---
+++ webpack.common.js
@@ -0,0 +1,32 @@
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+export default {
+  output: {
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
